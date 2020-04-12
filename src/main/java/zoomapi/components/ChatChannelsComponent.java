44d66e84e89b5c7d5b4ea427a889ff52e4f4c91e
Danny7226 <dchai2@uci.edu>
2020-04-11 23:18:16 -0700

    revise components

--- src/main/java/zoomapi/components/ChatChannelsComponent.java
+++ src/main/java/zoomapi/components/ChatChannelsComponent.java
@@ -3,4 +3,7 @@ package zoomapi.components;
 import zoomapi.utils.ApiClient;
 
 public class ChatChannelsComponent extends ApiClient{
+    public ChatChannelsComponent(String base_uri, int timeout){
+        super(base_uri, timeout);
+    }
 }
