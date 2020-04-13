dc1ec04e682d6243672b05df84cf322ecdf01e93
Danny7226 <dchai2@uci.edu>
2020-04-13 11:28:08 -0700

    remove unnesseary lib

--- src/main/java/zoomapi/utils/ApiClient.java
+++ src/main/java/zoomapi/utils/ApiClient.java
@@ -8,15 +8,12 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
 
 
