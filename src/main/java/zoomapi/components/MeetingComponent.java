90dee57928cd77188ba6eaac35c5d213461a5b0b
Danny7226 <duochai9611@gmail.com>
2020-04-20 00:07:54 -0700

    Exception Improved, Throttle mechanism implemented (#11)
    * exception raising system improved
    
    * Throttle mechanism implemented

--- src/main/java/zoomapi/components/MeetingComponent.java
+++ src/main/java/zoomapi/components/MeetingComponent.java
@@ -13,31 +13,31 @@ public class MeetingComponent extends BaseComponent{
         super(base_uri, config);
     }
 
-    public JSONObject listMeetings(Map<String,String> params) throws IOException {
+    public JSONObject listMeetings(Map<String,String> params){
         String[] keys = new String[] {"userId"};
         Util.requireKeys(params, keys);
         return this.getRequest(String.format("/users/%s/meetings", params.get("userId")), params);
     }
 
-    public JSONObject createMeeting(Map<String,String> params) throws IOException {
+    public JSONObject createMeeting(Map<String,String> params){
         String[] keys = new String[] {"userId", "start_time"}; //Scheduled Meeting
         Util.requireKeys(params, keys);
         return this.postRequest(String.format("/users/%s/meetings", params.get("userId")), params);
     }
 
-    public JSONObject getMeeting(Map<String,String> params) throws IOException {
+    public JSONObject getMeeting(Map<String,String> params){
         String[] keys = new String[] {"meetingId"};
         Util.requireKeys(params, keys);
         return this.getRequest(String.format("/meetings/%s", params.get("meetingId")), params);
     }
 
-    public JSONObject updateMeeting(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject updateMeeting(Map<String,String> params){
         String[] keys = new String[] {"meetingId", "start_time"};
         Util.requireKeys(params, keys);
         return this.patchRequest(String.format("/meetings/%s", params.get("meetingId")), params);
     }
 
-    public JSONObject deleteMeeting(Map<String,String> params) throws IOException {
+    public JSONObject deleteMeeting(Map<String,String> params){
         String[] keys = new String[] {"meetingId"};
         Util.requireKeys(params, keys);
         return this.deleteRequest(String.format("/meetings/%s", params.get("meetingId")), params);
--- src/main/java/zoomapi/components/WebinarComponent.java
+++ src/main/java/zoomapi/components/WebinarComponent.java
@@ -13,43 +13,43 @@ public class WebinarComponent extends BaseComponent {
         super(base_uri, config);
     }
 
-    public JSONObject listWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject listWebinar(Map<String,String> params){
         String[] keys = new String[] {"userId"};
         Util.requireKeys(params, keys);
         return this.getRequest(String.format("/users/%s/webinars", params.get("userId")), params);
     }
 
-    public JSONObject createWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject createWebinar(Map<String,String> params){
         String[] keys = new String[] {"userId"};
         Util.requireKeys(params, keys);
         return this.postRequest(String.format("/users/%s/webinars", params.get("userId")), params);
     }
 
-    public JSONObject updateWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject updateWebinar(Map<String,String> params){
         String[] keys = new String[] {"webinarId"};
         Util.requireKeys(params, keys);
         return this.patchRequest(String.format("/webinars/%s", params.get("webinarId")), params);
     }
 
-    public JSONObject deleteWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject deleteWebinar(Map<String,String> params){
         String[] keys = new String[] {"webinarId"};
         Util.requireKeys(params, keys);
         return this.deleteRequest(String.format("/webinars/%s", params.get("webinarId")), params);
     }
 
-    public JSONObject endWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject endWebinar(Map<String,String> params){
         String[] keys = new String[] {"webinarId"};
         Util.requireKeys(params, keys);
         return this.putRequest(String.format("/webinars/%s/status", params.get("webinarId")), params);
     }
 
-    public JSONObject getWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject getWebinar(Map<String,String> params){
         String[] keys = new String[] {"webinarId"};
         Util.requireKeys(params, keys);
         return this.getRequest(String.format("/webinars/%s", params.get("webinarId")), params);
     }
 
-    public JSONObject registerWebinar(Map<String,String> params) throws IOException, URISyntaxException {
+    public JSONObject registerWebinar(Map<String,String> params){
         String[] keys = new String[] {"webinarId", "email", "first_name", "last_name"};
         Util.requireKeys(params, keys);
         return this.postRequest(String.format("/webinars/%s/registrants", params.get("webinarId")), params);
