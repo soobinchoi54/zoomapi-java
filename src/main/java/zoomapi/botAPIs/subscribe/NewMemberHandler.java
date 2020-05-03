f56eb7e95775132e8e5cd1ba6a76bb2aed5beff5
Danny7226 <dchai2@uci.edu>
2020-05-03 16:41:58 -0700

    Duo (#58)
    * observer mechanism setup

---
+++ src/main/java/zoomapi/botAPIs/subscribe/NewMemberHandler.java
@@ -0,0 +1,8 @@
+package zoomapi.botAPIs.subscribe;
+
+public class NewMemberHandler extends CommandEventHandler{
+    @Override
+    protected void execute() {
+
+    }
+}
---
+++ src/main/java/zoomapi/botAPIs/subscribe/UpdatedMessageHandler.java
@@ -0,0 +1,8 @@
+package zoomapi.botAPIs.subscribe;
+
+public class UpdatedMessageHandler extends CommandEventHandler{
+    @Override
+    protected void execute() {
+
+    }
+}
