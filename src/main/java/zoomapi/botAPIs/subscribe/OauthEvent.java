403c7c552542af0cf306531df732317a8647fcec
Soobin Choi <soobinchoi54@gmail.com>
2020-05-04 03:13:35 -0700

    NewMembersEvent implementation

--- src/main/java/zoomapi/botAPIs/subscribe/OauthEvent.java
+++ src/main/java/zoomapi/botAPIs/subscribe/OauthEvent.java
@@ -1,6 +1,7 @@
 package zoomapi.botAPIs.subscribe;
 
 import zoomapi.OauthZoomClient;
+import zoomapi.botAPIs.OauthChannel;
 import zoomapi.botAPIs.OauthMessage;
 
 import java.net.PasswordAuthentication;
@@ -8,9 +9,11 @@ import java.net.PasswordAuthentication;
 public abstract class OauthEvent extends Event {
     protected OauthZoomClient client;
     protected OauthMessage oauthMessage;
+    protected OauthChannel oauthChannel;
     public OauthEvent(OauthZoomClient client){
         super();
         this.client = client;
         oauthMessage = new OauthMessage(client);
+        oauthChannel = new OauthChannel(client);
     }
 }
