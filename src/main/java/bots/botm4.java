b09a7c038b10f2406d8cfe32712a2699c37d02d4
Danny7226 <dchai2@uci.edu>
2020-05-10 17:01:05 -0700

    enhancement

--- src/main/java/bots/botm4.java
+++ src/main/java/bots/botm4.java
@@ -77,12 +77,14 @@ public class botm4 {
             client = new OauthZoomClient(client_id, client_secret, PORT, redirect_url, browser_path);
             SubscribeAgency.initialize();
             ChannelObserver observer = new ChannelObserver("Soobin on new messages", client, "updated channel");
-            ChannelObserver observer2 = new ChannelObserver("Duo on updates", client, "updated channel");
-            ChannelObserver observer3 = new ChannelObserver("Soobin on new members", client, "updated channel");
             observer.setObservingDate("2020-05-10", "2020-05-10");
             observer.subscribeTo(SubscribeAgency.NOTIFY_NEW_MESSAGES);
+
+            ChannelObserver observer2 = new ChannelObserver("Duo on updates", client, "updated channel");
             observer2.setObservingDate("2020-05-10", "2020-05-10");
             observer2.subscribeTo(SubscribeAgency.NOTIFY_MESSAGE_UPDATES);
+
+            ChannelObserver observer3 = new ChannelObserver("Soobin on new members", client, "updated channel");
             observer3.setObservingDate("2020-05-10", "2020-05-10");
             observer3.subscribeTo(SubscribeAgency.NOTIFY_NEW_MEMBERS);
 
