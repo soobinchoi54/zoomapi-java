4101680f53472db4d087d2460c1046369c1be943
Danny7226 <duochai9611@gmail.com>
2020-05-31 18:20:24 -0700

    cache improvement (#84)
    * cache improved
    
    * deprecate Unit

--- src/main/java/lib/cache/databaseData/Unit.java
+++ src/main/java/lib/cache/databaseData/Unit.java
@@ -1,5 +1,8 @@
 package lib.cache.databaseData;
 
+/***********************************
+ * Deprecated, use CacheUnit instead
+ * ********************************/
 @Deprecated
 public abstract class Unit {
     abstract public String toString();
